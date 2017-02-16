4d765c12d5ccf48ad94068561d14cf92d5de3bf4
Adam Cox <mr.adamcox@gmail.com>
2017-02-16 11:15:37 -0600

    add and register initial Stair model

--- stairdb/admin.py
+++ stairdb/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from models import Stair
 
-# Register your models here.
+admin.site.register(Stair)
--- stairdb/models.py
+++ stairdb/models.py
@@ -1,5 +1,31 @@
 from __future__ import unicode_literals
 
-from django.db import models
+from django.contrib.gis.db import models
+from django.core.serializers import serialize
 
-# Create your models here.
+class Stair(models.Model):
+    
+    TYPE_CHOICES = (
+        ('Alley Stairs','Alley Stairs'),
+        ('Building Access','Building Access'),
+        ('Country Park Stairs','Country Park Stairs'),
+        ('Curb Stairs','Curb Stairs'),
+        ('Elevated Walkway','Elevated Walkway'),
+        ('Escalator','Escalator'),
+        ('Footpath','Footpath'),
+        ('Maintenance Stairs','Maintenance Stairs'),
+        ('Pier Stairs','Pier Stairs'),
+        ('Park Stairs','Park Stairs'),
+        ('Plaza Stairs','Plaza Stairs'),
+        ('Street Stairs','Street Stairs'),
+        ('Subway','Subway'),
+    )
+
+    stairid = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=25,choices=TYPE_CHOICES,null=True,)
+    location = models.CharField(max_length=100)
+    geom = models.PointField()
+    
+    def __str__(self):
+        return str(self.stairid)
