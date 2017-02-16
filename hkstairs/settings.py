3fd5f381d4d6969d0da311a48bb704d23de15d93
Adam Cox <mr.adamcox@gmail.com>
2017-02-16 14:24:52 -0600

    add django-leaflet integration

--- hkstairs/settings.py
+++ hkstairs/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
+    'leaflet',
     'stairdb',
 ]
 
@@ -51,6 +53,12 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'hkstairs.urls'
+
+LEAFLET_CONFIG = {
+'DEFAULT_ZOOM': 12,
+'MIN_ZOOM': 3,
+'MAX_ZOOM': 18,
+}
 
 TEMPLATES = [
     {
