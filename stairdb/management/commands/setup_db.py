4a34eed268f0cfc6c48abdc9299d94b5c6324e99
Adam Cox <mr.adamcox@gmail.com>
2017-02-16 14:28:52 -0600

    create new command to drop/recreate db, make superuser, run migrations

---
+++ stairdb/management/commands/setup_db.py
@@ -0,0 +1,58 @@
+from django.core import management
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth.models import User
+from django.conf import settings
+import psycopg2 as db
+import os
+
+
+class Command(BaseCommand):
+    help = 'drops and recreates the app database.'
+
+    def add_arguments(self, parser):
+        # parser.add_argument(
+            # '-f',
+            # '--flush',
+            # action='store_true',
+            # dest='flush',
+            # default=False,
+            # help='delete all existing stairs before loading process',
+        # )
+        pass
+
+    def handle(self, *args, **options):
+
+        # if options['flush']:
+            # self.remove_stairs()
+            
+        self.setup_db()
+
+        
+    def setup_db(self):
+        print("Wiping database")
+        dbinfo = settings.DATABASES['default']
+
+        # Postgres version
+        conn = db.connect(host=dbinfo['HOST'], user=dbinfo['USER'],
+                        password=dbinfo['PASSWORD'], port=int(dbinfo['PORT']))
+        conn.autocommit = True
+        cursor = conn.cursor()
+        cursor.execute("DROP DATABASE " + dbinfo['NAME'])
+        cursor.execute("CREATE DATABASE " + dbinfo['NAME'] + " WITH ENCODING 'UTF8'")
+        
+        management.call_command('makemigrations')
+        management.call_command('migrate')
+
+        print("making admin...")
+        
+        default_user = User.objects.create_user('admin','','stairmaster')
+        default_user.is_staff = True
+        default_user.is_superuser = True
+        default_user.save()
+        
+        
+        print("admin superuser created. password = stairmaster.")
+        
+        
+        
+        print("done")
