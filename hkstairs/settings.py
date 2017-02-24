f8cf325e0f4d5ba6508e0074a86bae8fbf6d33dd
Adam Cox <mr.adamcox@gmail.com>
2017-02-23 18:20:45 -0600

    set path to new static dir

--- hkstairs/settings.py
+++ hkstairs/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
@@ -133,6 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,"stairdb","static"),
+]
 
 try:
     from settings_local import *
