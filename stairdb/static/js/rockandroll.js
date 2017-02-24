ee8a92ec2b6bce1f61b2373ecd92cb1cc001ff62
Adam Cox <mr.adamcox@gmail.com>
2017-02-24 15:42:36 -0600

    initial work on top navbar

---
+++ stairdb/static/js/rockandroll.js
@@ -0,0 +1,45 @@
+// resizing based on window width// media query event handler
+
+
+// resizing the header based on scroll bar position
+$(function(){
+    $('#header-nav').data('size','big');
+    $('#header-logo').data('size','big');
+});
+
+$(window).scroll(function(){
+    if($(document).scrollTop() > 0)
+    {
+        if($('#header-nav').data('size') == 'big')
+        {
+            $('#header-nav').data('size','small');
+            $('#header-nav').stop().animate({
+                height:'55px'
+            },600);
+        }
+        if($('#header-logo').data('size') == 'big')
+        {
+            $('#header-logo').data('size','small');
+            $('#header-logo').stop().animate({
+                height:'25px'
+            },600);
+        }
+    }
+    else
+    {
+        if($('#header-nav').data('size') == 'small')
+        {
+            $('#header-nav').data('size','big');
+            $('#header-nav').stop().animate({
+                height:'100px'
+            },600);
+        }
+        if($('#header-logo').data('size') == 'small')
+        {
+            $('#header-logo').data('size','big');
+            $('#header-logo').stop().animate({
+                height:'65px'
+            },600);
+        }        
+    }
+});
