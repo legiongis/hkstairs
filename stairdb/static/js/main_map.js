a104b19f46e9d3c2841418822f3f8d385796a7bb
Adam Cox <mr.adamcox@gmail.com>
2017-03-07 09:42:33 -0600

    rename map js file and remove old one

--- stairdb/static/js/main_map.js
+++ stairdb/static/js/main_map.js
@@ -1,54 +1,39 @@
-var dataurl = '/json/all';
-
-  window.addEventListener("map:init", function (event) {
+window.addEventListener("map:init", function (event) {
     var map = event.detail.map;
     map.addControl(new L.Control.Fullscreen());
-    
-    var markers = new L.MarkerClusterGroup({
-    showCoverageOnHover: false,
-    maxClusterRadius: 80
-});
 
-// Style function for thegeojson layer
-function getColor(status) {
-    if (status =='Pier Stairs'){return "#FF006F" }
-    else {return "#FCFF15"}
-}
-
-//loading the geojson for the power plants
-var geojsonAjax = new L.GeoJSON.AJAX(dataurl,{
-    style: function(feature) {
-        return {
-            fillColor: getColor(feature.properties.Type),
-            color: getColor(feature.properties.Type),
-            weight: 1,
-            opacity: 1,
-            fillOpacity: 0.7
-        }
-    },
-    // pointToLayer: function (feature, latlng) {
-        // return L.circleMarker(latlng)}});
-    // }
-});
-geojsonAjax.on('data:loaded', function () {
-    markers.addLayer(geojsonAjax);
-    map.addLayer(markers);
-});
-    
-    
-    
-    
-    
-    // fetch(dataurl)
-      // .then(function(resp) {
-        // return resp.json();
-      // })
-      // .then(function(data) {
-        // L.geoJson(data, {
+    var markers = new L.MarkerClusterGroup();
+    var polygons = new L.geoJson();
+    $.ajaxSetup( { "async": false } );  // remove line for (slow) async behaviour
+    $.getJSON('/json/all', function(pois) {
+        
+        for (var i in pois.features) {
+            var props = pois.features[i].properties;
+            var marker = new L.Marker(new L.LatLng(props.coords_y, props.coords_x));
+            var polygon = new L.Polygon(pois.features[i].geometry.coordinates[0]);
+            polygons.addData(pois.features[i]);
+            marker.bindPopup(props.name);
+            markers.addLayer(marker);
+            
+            // L.geoJson(data, {
           // onEachFeature: function onEachFeature(feature, layer) {
             // var props = feature.properties;
             // var content = `<img width="300" src="${props.picture_url}"/><h3>${props.title}</h3><p>${props.description}</p>`;
             // layer.bindPopup(content);
         // }}).addTo(map);
-      // });
-  });
+        // });
+}});
+    $.ajaxSetup( { "async": true } );   // remove line for (slow) async behaviour
+    map.addLayer(markers);
+    map.on('zoomend', function () {
+        console.log(map.getZoom());
+        if (map.getZoom() > 17)
+        {
+            map.addLayer(polygons);
+            map.removeLayer(markers);
+        } else {
+            map.removeLayer(polygons);
+            map.addLayer(markers);
+        }
+    }); 
+});
