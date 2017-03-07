25f142eb7845c4bf52b737aa8871407292b9bd80
Adam Cox <mr.adamcox@gmail.com>
2017-03-07 09:41:38 -0600

    override mini map setting in admin map

--- stairdb/admin.py
+++ stairdb/admin.py
@@ -3,6 +3,16 @@ from leaflet.admin import LeafletGeoAdmin
 from django.contrib import admin
 from models import Stair
 
-admin.site.register(Stair,LeafletGeoAdmin)
+class OverrideLeafletGeoAdmin(LeafletGeoAdmin):
+# straight hint @https://github.com/makinacorpus/django-leaflet/pull/28#issuecomment-23943492
+    settings_overrides = {
+        'TILES': [
+            # base layers by preference
+            ('OSM', 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', '<a href="http://www.openstreetmap.org/copyright" target="_blank"> OpenStreetMap</a> contributors'),
+        ],    
+        'MINIMAP': False, # <--- here
+}
+
+admin.site.register(Stair,OverrideLeafletGeoAdmin)
 #admin.site.register(Stair)
 
