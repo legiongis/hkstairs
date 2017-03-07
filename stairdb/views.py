8577ab5041199ce27856b296ee32b0150de12dba
Adam Cox <mr.adamcox@gmail.com>
2017-03-07 09:46:51 -0600

    use original serializer in favor of custom one

--- stairdb/views.py
+++ stairdb/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse
 from django.contrib.gis.db.models.functions import Centroid,AsGeoJSON
 from django.core.serializers import serialize
 import json
-from geojson import Feature
 from models import Stair
 
 def index(request):
@@ -16,44 +15,22 @@ def get_stairs(self,stairid="all"):
         q1 = Stair.objects.all()
 
     else:
-        q1 = [Stair.objects.get(stairid=stairid),Stair.objects.get(stairid=str(int(stairid)+1))]
+        #q1 = [Stair.objects.get(stairid=stairid),Stair.objects.get(stairid=str(int(stairid)+1))]
         q1 = [Stair.objects.get(stairid=stairid)]
-        
-
-    # space_qs = Stair.objects.all()
-    # centroid_result = Stair.objects.annotate(json=AsGeoJSON(Centroid('geom'))).all()
-
-    # att = space_qs.values()[0]
-
-    # do NOT include the original polygon geometry, so set it to None
-    # if att['geom']:
-        # att['geom'] = None
-
-    # res = Feature(geometry=json.loads(centroid_result), properties=att)
-    # print res
     
     jsond = serialize('geojson',q1,geometry_field="geom",
-          fields=('name','type'))
-          
-    centroid = False
-
-    super_serial = {
-        'type': "FeatureCollection",
-        # 'crs': {
-            # 'type': "name",
-            # 'properties': {
-                # 'name': "EPSG:4326"
-            # }
-        # },
-        'features': []
-    }
+          fields=('name','type','coords_x','coords_y'))
     
-    for a in q1:
-        super_serial['features'].append(a.as_json(centroid))
+    ## custom serializing code is currently not in use
+    # centroid = False
+    # super_serial = {
+        # 'type': "FeatureCollection",
+        # 'features': []
+    # }
+    
+    # for a in q1:
+        # super_serial['features'].append(a.as_json(centroid))
         
-    ret = json.dumps(super_serial)
-    # print super_serial
+    # ret = json.dumps(super_serial)
 
-    
-    #return JsonResponse(json, safe=False)
-    return HttpResponse(ret, content_type='application/json')
+    return HttpResponse(jsond, content_type='application/json')
