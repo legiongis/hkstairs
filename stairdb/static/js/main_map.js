b745987c4d0badb42bc4b18a0ede028b50eb937f
Adam Cox <mr.adamcox@gmail.com>
2017-04-12 18:39:20 -0500

    add spinner to run during ajax load

--- stairdb/static/js/main_map.js
+++ stairdb/static/js/main_map.js
@@ -1,11 +1,38 @@
+$(window).load(function () {
+    var isIE = /*@cc_on!@*/false || !!document.documentMode;
+    if (isIE) {
+        $("#loader-content").html("<p>Internet Explorer not supported.<br>Use Chrome, FireFox, or Edge.</p>");
+        $("#loader-content").addClass("loader-mask");
+    }
+});
+
 window.addEventListener("map:init", function (event) {
     var map = event.detail.map;
     map.addControl(new L.Control.Fullscreen());
 
     var markers = new L.MarkerClusterGroup();
     var polygons = new L.geoJson();
-    $.ajaxSetup( { "async": false } );  // remove line for (slow) async behaviour
+    //$.ajaxSetup( { "async": false } );  // remove line for (slow) async behaviour
+    
+    // --- EXAMPLE of using a general ajax request instead of .getJSON. May be necessary down the road. ---
+    // $.ajax({
+      // url: "/json/all",
+      // cache: false,
+      // dataType: "json",
+      // success: function(data) {
+        // var request_time = new Date().getTime() - start_time;
+        // console.log(".ajax time");
+        // console.log(request_time);
+      // },
+      // error: function (request, status, error) { alert(status + ", " + error); }
+    // });
+
+    
     $.getJSON('/json/all', function(pois) {
+        
+        
+        var start_time = new Date().getTime();
+        
         
         for (var i in pois.features) {
             var props = pois.features[i].properties;
@@ -22,8 +49,16 @@ window.addEventListener("map:init", function (event) {
             // layer.bindPopup(content);
         // }}).addTo(map);
         // });
-}});
-    $.ajaxSetup( { "async": true } );   // remove line for (slow) async behaviour
+        }
+        success:
+            $("#loader").addClass("hidden");
+            var request_time = new Date().getTime() - start_time;
+            console.log("getJSON request time:");
+            console.log(request_time);
+    });
+    
+    //$.ajaxSetup( { "async": true } );   // remove line for (slow) async behaviour
+    
     map.addLayer(markers);
     map.on('zoomend', function () {
         console.log(map.getZoom());
@@ -35,5 +70,5 @@ window.addEventListener("map:init", function (event) {
             map.removeLayer(polygons);
             map.addLayer(markers);
         }
-    }); 
+    });
 });
