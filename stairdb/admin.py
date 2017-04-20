98f4ea1f4836bc8bdcf0a3fe106c113e1e3f4c64
Adam Cox <mr.adamcox@gmail.com>
2017-04-20 15:37:27 -0500

    add search ability to stair admin page

--- stairdb/admin.py
+++ stairdb/admin.py
@@ -4,15 +4,17 @@ from django.contrib import admin
 from models import Stair
 
 class OverrideLeafletGeoAdmin(LeafletGeoAdmin):
-# straight hint @https://github.com/makinacorpus/django-leaflet/pull/28#issuecomment-23943492
+    
+    # straight hint @https://github.com/makinacorpus/django-leaflet/pull/28#issuecomment-23943492
     settings_overrides = {
         'TILES': [
             # base layers by preference
             ('OSM', 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', '<a href="http://www.openstreetmap.org/copyright" target="_blank"> OpenStreetMap</a> contributors'),
         ],    
         'MINIMAP': False, # <--- here
-}
+    }
 
-admin.site.register(Stair,OverrideLeafletGeoAdmin)
-#admin.site.register(Stair)
+class StairAdmin(OverrideLeafletGeoAdmin):
+    search_fields = ['stairid','type','location']
 
+admin.site.register(Stair,StairAdmin)
