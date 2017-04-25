492a8b3ceb26dcdf464d0fcbdb194dbcd3a518b9
Adam Cox <mr.adamcox@gmail.com>
2017-04-24 22:27:49 -0500

    update urls to manage static files
    Signed-off-by: Adam Cox <mr.adamcox@gmail.com>

--- hkstairs/urls.py
+++ hkstairs/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('stairdb.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
