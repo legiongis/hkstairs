31386a2ac02c5b1216c76ba41f4f1869b464ab65
Adam Cox <mr.adamcox@gmail.com>
2017-06-07 15:06:52 -0500

    format code, remove unused line
    Signed-off-by: Adam Cox <mr.adamcox@gmail.com>

--- stairdb/static/js/admin-map.js
+++ stairdb/static/js/admin-map.js
@@ -1,7 +1,6 @@
 window.addEventListener("map:init", function (event) {
 
     var map = event.detail.map;
-    // map.addControl(new L.Control.Fullscreen());
     
     var stairpointsLayer = L.tileLayer.wms("http://legiongis.com/geoserver/hkstairs/wms", {
       layers: 'hkstairs:Stairs_v5points_wgs84',
@@ -13,7 +12,10 @@ window.addEventListener("map:init", function (event) {
       zIndex:10,
     }).addTo(map);
     
-    var osm_minimap = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token='+mapbox_api_key,{maxNativeZoom:18,maxZoom:19})
+    var osm_minimap = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token='+mapbox_api_key,{
+        maxNativeZoom:18,
+        maxZoom:19
+    });
     
     map.addControl(new L.Control.MiniMap(osm_minimap));
 });
