c4c910caea93c81a7de2fb9a8957edbdb535d9e6
adam cox <mr.adamcox@gmail.com>
2017-09-28 16:13:53 -0500

    fix reference to labeled stairs WMS
    Signed-off-by: adam cox <mr.adamcox@gmail.com>

--- stairdb/static/js/admin-map.js
+++ stairdb/static/js/admin-map.js
@@ -2,16 +2,17 @@ window.addEventListener("map:init", function (event) {
 
     var map = event.detail.map;
     
-    var stairpointsLayer = L.tileLayer.wms("http://legiongis.com/geoserver/hkstairs/wms", {
-      layers: 'hkstairs:Stairs_v5points_wgs84',
+    var stairpointsLayer = L.tileLayer.wms("https://db.legiongis.com/geoserver/ows", {
+      layers: 'hkstairs:Stairs_v5_points_wgs84',
       format: 'image/png',
       transparent: true,
       version: '1.1.1',
       attribution: "",
       maxZoom:19,
       zIndex:10,
+	  tiled:false,
     }).addTo(map);
-    
+
     var osm_minimap = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token='+mapbox_api_key,{
         maxNativeZoom:18,
         maxZoom:19
