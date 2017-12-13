649394fa1ea240ff059b9c9e681b093c4ca32e50
adam cox <mr.adamcox@gmail.com>
2017-12-13 14:36:19 -0600

    add command to refresh the cache for the json used in the map

---
+++ stairdb/management/commands/refresh_map_cache.py
@@ -0,0 +1,30 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+import os
+import urllib2
+
+class Command(BaseCommand):
+    help = 'refreshes the cache'
+
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        self.refresh_map_cache()
+        
+    def refresh_map_cache(self,table_name='',verbose=False):
+    
+        cache_dir = settings.CACHES['default']['LOCATION']
+        for f in os.listdir(cache_dir):
+            if f.endswith(".djcache"):
+                os.remove(os.path.join(cache_dir,f))
+        
+        req = urllib2.Request(url='http://localhost:8000/stair/?format=json')
+        
+        ## this header needs to be added to correctly mimic the ajax requests
+        ## that are made in the map js. if absent, django will make a new
+        ## cache file next time the url is requested, and will ignore the one
+        ## created below
+        req.add_header('Accept', 'application/json, text/javascript, */*; q=0.01')
+        
+        response = urllib2.urlopen(req)
