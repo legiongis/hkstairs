721458246147590535507456836633d58c3de71e
Adam Cox <mr.adamcox@gmail.com>
2026-06-17 11:27:39 -0500

    update static files settings; remove duped path

--- hkstairs/settings.py
+++ hkstairs/settings.py
@@ -135,14 +135,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
-
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,"stairdb","static"),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LEAFLET_CONFIG = {
     'DEFAULT_CENTER': (22.28,114.15),
